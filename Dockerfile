d0fe1c78326b85d547035491251452752a6cc2c6
c-bata <contact@c-bata.link>
2019-02-23 02:38:37 +0900

    UNIX philosophy: Make each actions do one thing well

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,9 @@ ADD ./main.go /app/main.go
 RUN go build -o ingress_rules_editor ./main.go
 
 # Runner
-FROM gcr.io/cloud-builders/kubectl@sha256:abeaf7bd496e66301f92fd884683feaec6894bfb12d1a02fdbc98a920fab4968
+FROM alpine:latest
+
+RUN apk --update add ca-certificates atop
 
 LABEL "name"="ingress-rules-editor"
 LABEL "version"="0.0.1"
@@ -22,15 +24,6 @@ LABEL "com.github.actions.description"="Edit kubernetes ingress rules."
 LABEL "com.github.actions.icon"="upload-cloud"
 LABEL "com.github.actions.color"="green"
 
-ENV DOCKERVERSION=18.06.1-ce
-RUN apt-get update && apt-get -y --no-install-recommends install curl \
-  && curl -fsSLO https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKERVERSION}.tgz \
-  && tar xzvf docker-${DOCKERVERSION}.tgz --strip 1 \
-                 -C /usr/local/bin docker/docker \
-  && rm docker-${DOCKERVERSION}.tgz \
-  && rm -rf /var/lib/apt/lists/*
-
-COPY --from=builder /app/ingress_rules_editor /builder/ingress_rules_editor
-
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,4 @@
 #!/bin/sh
 set -e
 
-BRANCH_NAME=$(echo $GITHUB_REF | sed -e "s/^refs\/heads\///g")
-echo $BRANCH_NAME
-
-FEATURE_NAME=$(echo $BRANCH_NAME | sed -e "s/^${BRANCH_PREFIX}//g")
-echo $FEATURE_NAME
-
-INGRESS_HOST="$FEATURE_NAME"
-if [ "$HOST_PREFIX" != "" ] && [ "$HOST_SUFFIX" != "" ]; then
-  INGRESS_HOST="${HOST_PREFIX}${FEATURE_NAME}${HOST_SUFFIX}"
-elif [ "$HOST_SUFFIX" != "" ]; then
-  INGRESS_HOST="${FEATURE_NAME}${HOST_SUFFIX}"
-elif [ "$HOST_PREFIX" != "" ]; then
-  INGRESS_HOST="${HOST_PREFIX}${FEATURE_NAME}"
-fi
-echo $INGRESS_HOST
-
-SERVICE_NAME="$FEATURE_NAME"
-if [ "$SERVICE_PREFIX" != "" ] && [ "$SERVICE_SUFFIX" != "" ]; then
-  SERVICE_NAME="${SERVICE_PREFIX}${FEATURE_NAME}${SERVICE_SUFFIX}"
-elif [ "$SERVICE_SUFFIX" != "" ]; then
-  SERVICE_NAME="${FEATURE_NAME}${SERVICE_SUFFIX}"
-elif [ "$SERVICE_PREFIX" != "" ]; then
-  SERVICE_NAME="${SERVICE_PREFIX}${FEATURE_NAME}"
-fi
-echo $SERVICE_NAME
-
-/builder/ingress_rules_editor ${OPERATION} -ingress="$INGRESS_NAME" -host="$INGRESS_HOST" -service=$"SERVICE_NAME" -port=$"SERVICE_PORT" -path="$INGRESS_PATH" -namespace="$NAMESPACE"
+sh -c "/builder/ingress_rules_editor $*"
